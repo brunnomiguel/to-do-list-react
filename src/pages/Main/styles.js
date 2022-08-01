66f8df873df387358598ec621d7bf32af406ffbb
Brunno Miguel <miguel.brunno06@gmail.com>
2022-08-01 17:24:55 -0300

    feat: tasks component

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,5 +3,12 @@ import styled, { css } from "styled-components";
 export const Container = styled.main`
   ${css`
     width: 100%;
+
+    ul {
+      max-width: 600px;
+      width: 100%;
+      margin: 0 auto;
+      padding: 20px;
+    }
   `}
 `;
